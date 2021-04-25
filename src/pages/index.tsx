138f82ba20aa89a8b700f98d51bf87d129eff256
Denison Amaral <72951419+denisonamaral@users.noreply.github.com>
2021-04-25 02:19:54 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Final | ig.news</title>
+      </Head>
+      <h1>Hello World Next!</h1>
+    </>
+  );
+}
